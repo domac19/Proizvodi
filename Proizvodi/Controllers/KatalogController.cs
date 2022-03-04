8598deae2f953adc2f3811d94660d30fa51d47f5
Domagoj Licitar <licitar.domagoj@gmail.com>
2022-03-04 15:05:27 +0100

    Add project files.

---
+++ Proizvodi/Controllers/KatalogController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Proizvodi.Controllers
+{
+    public class KatalogController : ApiController
+    {
+        private ProizvodiEntities _proizvodiEntities;
+        public KatalogController()
+        {
+            _proizvodiEntities = new ProizvodiEntities();
+        }
+
+        // GET/api/katalog
+        public IHttpActionResult GetKatalog(string query = null)
+        {
+            var proizvodKatalog = _proizvodiEntities.KatalogProizvodas.ToList();
+
+            return Ok(proizvodKatalog);
+        }
+
+        // GET/katalog/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var katalog = _proizvodiEntities.KatalogProizvodas.SingleOrDefault(c => c.KatalogId == id);
+
+            if (katalog == null)
+                return NotFound();
+
+            return Ok(katalog);
+        }
+
+        // POST/api/katalog
+        [HttpPost]
+        public IHttpActionResult CreateProizvod(KatalogProizvoda katalogProizvoda)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _proizvodiEntities.KatalogProizvodas.Add(katalogProizvoda);
+            _proizvodiEntities.SaveChanges();
+
+            return Ok(katalogProizvoda);
+        }
+
+        // PUT/api/proizvodi
+        public void UpdateCustomer(int id, KatalogProizvoda katalogProizvoda)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var katalogDb = _proizvodiEntities.KatalogProizvodas.SingleOrDefault(c => c.KatalogId == id);
+
+            if (katalogDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _proizvodiEntities.SaveChanges();
+        }
+
+        // DELETE/api/proizvod/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var katalogDb = _proizvodiEntities.KatalogProizvodas.SingleOrDefault(c => c.KatalogId == id);
+
+            if (katalogDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _proizvodiEntities.KatalogProizvodas.Remove(katalogDb);
+            _proizvodiEntities.SaveChanges();
+        }
+    }
+}
---
+++ Proizvodi/Controllers/ProizvodiController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Proizvodi.Controllers
+{
+    public class ProizvodiController : ApiController
+    {
+        private ProizvodiEntities _proizvodiEntities;
+        public ProizvodiController()
+        {
+            _proizvodiEntities = new ProizvodiEntities();
+        }
+
+        // GET/api/proizvodi
+        public IHttpActionResult GetCustomers(string query = null)
+        {
+            var proizvodi = _proizvodiEntities.Proizvods.ToList();
+
+            return Ok(proizvodi);
+        }
+
+        // GET/proizvod/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var proizvod = _proizvodiEntities.Proizvods.SingleOrDefault(c => c.ProizvodId == id);
+
+            if (proizvod == null)
+                return NotFound();
+
+            return Ok(proizvod);
+        }
+
+        // POST/api/proizvodi
+        [HttpPost]
+        public IHttpActionResult CreateProizvod(Proizvod proizvod)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _proizvodiEntities.Proizvods.Add(proizvod);
+            _proizvodiEntities.SaveChanges();
+
+            return Ok(proizvod);
+        }
+
+        // PUT/api/proizvodi
+        public void UpdateCustomer(int id, Proizvod proizvod)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var proizvodDb = _proizvodiEntities.Proizvods.SingleOrDefault(c => c.ProizvodId == id);
+
+            if (proizvodDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _proizvodiEntities.SaveChanges();
+        }
+
+        // DELETE/api/proizvod/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var proizvodDb = _proizvodiEntities.Proizvods.SingleOrDefault(c => c.ProizvodId == id);
+
+            if (proizvodDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _proizvodiEntities.Proizvods.Remove(proizvodDb);
+            _proizvodiEntities.SaveChanges();
+        }
+    }
+}
