a3310699f34490c86631669b56604746f14b7243
Domagoj Licitar <licitar.domagoj@gmail.com>
2022-03-07 09:37:14 +0100

    Added Controller and View for Proizvodi

---
+++ Proizvodi/Controllers/ProizvodsController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proizvodi;
+
+namespace Proizvodi.Controllers
+{
+    public class ProizvodsController : Controller
+    {
+        private ProizvodiEntities db = new ProizvodiEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Proizvods.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proizvod proizvod = db.Proizvods.Find(id);
+            if (proizvod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proizvod);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProizvodId,Naziv,Tip,Cijena")] Proizvod proizvod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proizvods.Add(proizvod);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(proizvod);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proizvod proizvod = db.Proizvods.Find(id);
+            if (proizvod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proizvod);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProizvodId,Naziv,Tip,Cijena")] Proizvod proizvod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proizvod).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(proizvod);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proizvod proizvod = db.Proizvods.Find(id);
+            if (proizvod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proizvod);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proizvod proizvod = db.Proizvods.Find(id);
+            db.Proizvods.Remove(proizvod);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
